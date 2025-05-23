d0198c77bf0c22f354dd04b04fe1acb035cbcca5
Omar Salem <124590406+osalem192@users.noreply.github.com>
2025-05-23 22:57:28 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,107 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Security Group
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_traffic"
+  description = "Allow all inbound and outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-ec2-key"              # Name of the key pair in AWS
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your public key file
+}
+
+# EC2 Instance
+resource "aws_instance" "public_ec2" {
+  ami           = "ami-0c614dee691cbbf37" # Amazon Linux 2 (for us-east-1)
+  instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.allow_all.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.my_key_pair.key_name
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "[web]" > inventory
+echo "${self.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/id_rsa" >> inventory
+EOT
+  }
+
+  tags = {
+    Name = "jenkins-EC2"
+  }
+}
